43e9e8bf20f96def6f13b518805f81205d75b4ea
Ahmed Alaa <ahma4352@gmail.com>
2024-03-20 06:15:25 +0200

    %improve link performance

--- src/Components/Link/Link.jsx
+++ src/Components/Link/Link.jsx
@@ -1,34 +1,43 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Link = ({ to, className,style,children,aria_current="page" , onClick}) => {
+const Link = ({
+  to,
+  className,
+  style,
+  children,
+  aria_current = "page",
+  onClick,
+}) => {
   const navigate = useNavigate();
   const handleClicked = () => {
-    const bars = document.getElementById('bars');
+    const bars = document.getElementById("bars");
 
     if (bars) {
-      bars.classList.add('show');
+      bars.classList.add("show");
 
       setTimeout(() => {
-        bars.classList.remove('show');
-        bars.classList.add('hide');
         navigate(to);
+      }, 50);
+      setTimeout(() => {
+        bars.classList.remove("show");
+        bars.classList.add("hide");
       }, 400);
 
-      setTimeout(() => bars.classList.remove('hide'), 1600);
+      setTimeout(() => bars.classList.remove("hide"), 1600);
     }
-    
   };
 
   return (
-    <a style={style}
+    <a
+      style={style}
       className={className}
-      onClick={(e)=>{
-        handleClicked();// call handleClicked function to control navigation bar when click on it
-        onClick&&onClick(e) // if onClick is not undefined, call it =>Change style for nav element
+      onClick={(e) => {
+        handleClicked(); // call handleClicked function to control navigation bar when click on it
+        onClick && onClick(e); // if onClick is not undefined, call it =>Change style for nav element
       }}
-      href='#'
-      aria_current={aria_current!=="page"?aria_current:"page"}
+      href="#"
+      aria_current={aria_current !== "page" ? aria_current : "page"}
     >
       {children}
     </a>
